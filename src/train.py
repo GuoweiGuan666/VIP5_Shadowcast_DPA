fd359fc329df3085246007350094e505b1066710
Guowei Guan <guanguowei2016@163.com>
2025-07-27 22:32:34 +0800

    save on 0727

--- src/train.py
+++ src/train.py
@@ -259,7 +259,7 @@ class Trainer(TrainerBase):
                 if avg_valid_loss < self.best_eval_loss:
                     self.best_eval_loss = avg_valid_loss
                     self.save("BEST_EVAL_LOSS")
-                    self.save("Current Best Epoch%02d" % (epoch))
+                    print("Current Best Epoch: ", epoch)
             else:
                 print(f"Epoch {epoch}: Skip validation")
                 self.save("Epoch%02d" % (epoch))
